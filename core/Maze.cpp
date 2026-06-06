31b5647140741c9ab3decfdecf82d56e2e52ae5e
CharlesAdewumi2004 <charlesadewumi2004@gmail.com>
2026-06-06 19:23:28 +0100

    fixed issue with maze interface not defining constructor and destructor

--- core/Maze.cpp
+++ core/Maze.cpp
@@ -1,12 +1,15 @@
 #include "Maze.h"
 
 
+Maze::Maze() = default;
+Maze::~Maze() = default;
+
 void Maze::AddRoom(std::shared_ptr<Room> newRoom){
     _rooms.push_back(newRoom);
 }
 
 std::shared_ptr<Room> Maze::RoomNo(int roomNum) const{
-    for(auto const room : _rooms){
+    for(auto const &room : _rooms){
         if(room.get()->GetRoomNumber() == roomNum){
             return room;
         }
